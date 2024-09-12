4ac3b879217c8dd2d4b07f22718ee4784d3cf440
Rembqq <nikitavolkovskyi@gmail.com>
2024-09-12 13:08:01 +0300

    team description and bio for web

--- src/main/java/com/example/springkpi/lab1/controller/HelloController.java
+++ src/main/java/com/example/springkpi/lab1/controller/HelloController.java
@@ -9,6 +9,16 @@ public class HelloController {
     @GetMapping("/")
     @ResponseBody
     String helloWorld() {
-        return "Hello, bud";
+        return "<header>About me</header>\n" +
+                "\n" +
+                "<main>\n" +
+                "  <p>\n" +
+                "    Our humble team contains of me and... only me. My name is Nikita Volkovskyi, nice to meet you ^^\n" +
+                "  </p>\n" +
+                "\n" +
+                    "  <p>I've been studying Java since the first semester, just didn't want to continue learning JS, C++ or Python even though I had a year of experience learning each language.</p>\n" +
+                "\n" +
+                "   <p>Recently started course on Coursera \"Algorithms\" by Princeton Uni with examples on Java. Seems interesting. Looking forward to learn more info about frameworks :></p>\n" +
+                "</main>";
     }
 }
